d95870e5cd89ad177522b9ed3b81a89355f719d3
Samuel Alves <samuel.a.souza@outlook.com>
2021-04-22 07:59:42 -0300

    Criations and stylization of players

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+import styles from './styles.module.scss';
+
+
+export function Header() {
+    const currentData = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    }); //Ver domentação no date-fns.org
+
+    return (
+        <header className={styles.headerContainer}>
+            <img src="/logo.svg" alt="Podcastr" />
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>{currentData}</span>
+
+        </header>
+    );
+}
